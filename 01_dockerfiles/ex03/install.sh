0bee1c7e621924e3d1ca1ec1dcfa7e859f5e6b52
Youssef Lagtab <yousseflagtab@gmail.com>
2019-11-10 23:34:41 +0100

    finished 01

--- 01_dockerfiles/ex03/install.sh
+++ 01_dockerfiles/ex03/install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-apt update \
-apt install vim curl tzdata -y \
-ln -fs /usr/share/zoneinfo/Africa/Casablanca /etc/localtime \
-dpkg-reconfigure -f noninteractive tzdata \
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash \
-apt install gitlab-ce -y \
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=MR" -keyout /etc/gitlab/ssl/gitlab.example.com.key -out /etc/gitlab/ssl/gitlab.example.com.crt \
-echo "Port 2222" > /etc/ssh/sshd_config
-echo "Port 2222" > /etc/ssh/ssh_config
-echo "external_url 'https://192.168.99.101'" >> /etc/gitlab/gitlab.rb
+apt update >> ~/logs
+DEBIAN_FRONTEND=noninteractive apt install vim curl tzdata -y >> ~/logs
+ln -fs /usr/share/zoneinfo/Africa/Casablanca /etc/localtime >> ~/logs
+dpkg-reconfigure -f noninteractive tzdata >> ~/logs
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash >> ~/logs
+apt install gitlab-ce -y >> ~/logs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=MR" -keyout /etc/gitlab/ssl/gitlab.example.com.key -out /etc/gitlab/ssl/gitlab.example.com.crt >> ~/logs
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 2222" >> /etc/ssh/ssh_config
+echo "external_url 'https://0.0.0.0'" >> /etc/gitlab/gitlab.rb
 echo "nginx['redirect_http_to_https'] = true" >> /etc/gitlab/gitlab.rb
 echo "registry_nginx['redirect_http_to_https'] = true" >> /etc/gitlab/gitlab.rb
 echo "mattermost_nginx['redirect_http_to_https'] = true" >> /etc/gitlab/gitlab.rb
-echo "letsencrypt['enable'] = nil" >> /tc/gitlab/gitlab.rb
-echo "gitlab_rails['gitlab_shell_ssh_port'] = 2222" >> /tc/gitlab/gitlab.rb
-/opt/gitlab/embedded/bin/runsvdir-start & \ #no initsystem
-gitlab-ctl reconfigure \
+echo "letsencrypt['enable'] = nil" >> /etc/gitlab/gitlab.rb
+echo "gitlab_rails['gitlab_shell_ssh_port'] = 2222" >> /etc/gitlab/gitlab.rb
+/opt/gitlab/embedded/bin/runsvdir-start >> ~/run_logs & #no initsystem
+gitlab-ctl reconfigure >> ~/logs
 gitlab-ctl tail &
